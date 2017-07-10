9bc53f151d0f1532dfbfb2857a1d11384eb51dea
Adrian Rodriguez <adrianwritenow@gmail.com>
2017-07-10 16:23:07 -0400

    finished answers

--- starter_files 2/main.js
+++ starter_files 2/main.js
@@ -7,46 +7,75 @@ console.log(data);
 
 
 // 1: Show me how to calculate the average price of all items.
-function question1 () {
+function question1() {
   let sum = 0;
-
-for (var i = 0; i < data.length; i++) {
-    sum = sum + data[i].price;
-}
-console.log(( sum / data.length).toFixed(2));
-  // Answer:
+  //
+  data.forEach(function(element) {
+    sum = sum + element.price;
+  });
+  console.log("The average price is $", (sum / data.length).toFixed(2));
 }
 
 
 
 // 2: Show me how to get an array of items that cost between $14.00 and $18.00 USD
-function question2 () {
-  // Answer:
+function question2() {
+  data.forEach(function(element) {
+    if (element.price <= 18 && element.price >= 14) {
+      console.log(element.title);
+    }
+  });
 }
 
 
 
 // 3: Which item has a "GBP" currency code? Display it's name and price.
-function question3 () {
+function question3() {
+  data.forEach(function(element) {
+    if (element.currency_code === "GBP") {
+      console.log(element.title, element.price, "pounds");
+    }
+  });
   // Answer:
 }
 
 
 // 4: Display a list of all items who are made of wood.
-function question4 () {
+function question4() {
+  data.forEach(function(element) {
+    element.materials.forEach(function(materials) {
+      if (materials === "wood") {
+        console.log(element.title, "is made of Wood.");
+      }
+    });
+  });
   // Answer:
 }
 
 
 // 5: Which items are made of eight or more materials?
 //    Display the name, number of items and the items it is made of.
-function question5 () {
+function question5() {
+  data.forEach(function(element) {
+    if (element.materials.length >= 8) {
+      console.log(element.title, element.materials);
+    }
+
+  });
   // Answer:
 }
 
 
 // 6: How many items were made by their sellers?
 // Answer:
-function question6 () {
+function question6() {
+  let iMade = 0;
+  data.forEach(function(element) {
+    if (element.who_made === "i_did") {
+      iMade++
+    }
+
+  })
+  console.log(iMade + " items were made by their sellers.")
   // Answer:
 }
